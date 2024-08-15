c4d23fbc9fdb9b4a940350f3585a60a212423dbf
User-Takhirov <tohirovfayruz@gmail.com>
2024-08-15 18:29:01 +0500

    js day 12

--- main.js
+++ main.js
@@ -46,3 +46,59 @@
 //   return count;
 // };
 // console.log(x(str));
+
+////////////////////////////////////////////////////////////////
+//hw
+//1
+// array ichidagi katta va kichik sonlrni topib beradigan funksiya
+// let arr = [89, 100, 12, 45, 32, 76];
+// const x = (arr) => {
+//   let max = arr[0];
+//   let min = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//   }
+//   return { max, min };
+// };
+// console.log(x(arr));
+
+//2
+// array ichidagi 2 dan katta bo'lgan sonlarni boshqa array ga ko'chirish
+//2dan katta bolgan sonlarniu push qilish
+// let arr = [9, 1, 3, 6, 2];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 2) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+
+//3
+//toq va juft sonlarni topish
+// let arr = [2, 6, 4, 5, 9];
+// let juft = [];
+// let toq = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     juft.push(arr[i]);
+//   } else {
+//     toq.push(arr[i]);
+//   }
+// }
+// console.log("juft",juft);
+// console.log("toq",toq);
+
+//4
+//sonlarning qiymatini hisoblash
+// let arr = [10, 5, 3, 2];
+// let newArr = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   newArr += arr[i];
+// }
+// console.log(newArr);
